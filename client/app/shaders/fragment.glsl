b5972d473ab73e97f14cad605cacf7abaaa9c814
James Hagood <cardfaux@tutanota.com>
2022-07-08 18:11:13 -0500

    first commit

---
+++ client/app/shaders/fragment.glsl
@@ -0,0 +1,11 @@
+varying vec2 vUv;
+varying float vNoise;
+
+void main()	{
+	// vec2 newUV = (vUv - vec2(0.5))*resolution.zw + vec2(0.5);
+	vec3 color1 = vec3(1., 0., 0.);
+	vec3 color2 = vec3(1., 1., 1.);
+	vec3 finalColor = mix(color1, color2, 0.5 * (vNoise + 1.));
+
+	gl_FragColor = vec4(finalColor,1.);
+}

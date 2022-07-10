e9952aa78b3378aee0ebdee2bb0b66c842990f44
James Hagood <cardfaux@tutanota.com>
2022-07-10 10:39:41 -0500

    added some more glsl code

--- client/app/index.js
+++ client/app/index.js
@@ -4,12 +4,18 @@ import * as THREE from 'three';
 import imagesLoaded from 'imagesloaded';
 import gsap from 'gsap';
 import FontFaceObserver from 'fontfaceobserver';
-import Scroll from './scroll';
+import Scroll from './utils/scroll';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import fragment from './shaders/fragment.glsl';
 import vertex from './shaders/vertex.glsl';
+import noise from './shaders/noise.glsl';
 
 import ocean from '../assets/ocean.jpg';
+
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
 
 export default class Sketch {
   constructor(options) {
@@ -29,6 +35,8 @@ export default class Sketch {
       antialias: true,
       alpha: true,
     });
+
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
     this.container.appendChild(this.renderer.domElement);
 
@@ -67,6 +75,7 @@ export default class Sketch {
       this.resize();
       this.setupResize();
       //this.addObjects();
+      this.composerPass();
       this.render();
       // window.addEventListener('scroll',()=>{
       //     this.currentScroll = window.scrollY;
@@ -74,6 +83,57 @@ export default class Sketch {
       // })
     });
   }
+
+  composerPass() {
+    this.composer = new EffectComposer(this.renderer);
+    this.renderPass = new RenderPass(this.scene, this.camera);
+    this.composer.addPass(this.renderPass);
+
+    //custom shader pass
+    var counter = 0.0;
+    this.myEffect = {
+      uniforms: {
+        tDiffuse: { value: null },
+        scrollSpeed: { value: null },
+        time: { value: null },
+      },
+      vertexShader: `
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix 
+            * modelViewMatrix 
+            * vec4( position, 1.0 );
+        }
+        `,
+      fragmentShader: `
+        uniform sampler2D tDiffuse;
+        varying vec2 vUv;
+        uniform float scrollSpeed;
+        uniform float time;
+        ${noise}
+        void main(){
+          vec2 newUV = vUv;
+          float area = smoothstep(1.,0.8,vUv.y)*2. - 1.;
+          float area1 = smoothstep(0.4,0.0,vUv.y);
+          area1 = pow(area1,4.);
+          float noise = 0.5*(cnoise(vec3(vUv*10.,time/5.)) + 1.);
+          float n = smoothstep(0.5,0.51, noise + area/2.);
+          newUV.x -= (vUv.x - 0.5)*0.1*area1*scrollSpeed;
+          gl_FragColor = texture2D( tDiffuse, newUV);
+        //   gl_FragColor = vec4(n,0.,0.,1.);
+        gl_FragColor = mix(vec4(1.),texture2D( tDiffuse, newUV),n);
+        // gl_FragColor = vec4(area,0.,0.,1.);
+        }
+        `,
+    };
+
+    this.customPass = new ShaderPass(this.myEffect);
+    this.customPass.renderToScreen = true;
+
+    this.composer.addPass(this.customPass);
+  }
+
   mouseMovement() {
     window.addEventListener(
       'mousemove',
@@ -129,7 +189,7 @@ export default class Sketch {
     this.imageStore = this.images.map((img) => {
       let bounds = img.getBoundingClientRect();
 
-      let geometry = new THREE.PlaneBufferGeometry(bounds.width, bounds.height, 10, 10);
+      let geometry = new THREE.PlaneGeometry(bounds.width, bounds.height, 10, 10);
       //let texture = new THREE.Texture(img);
       /*
       let image = new Image();
@@ -176,8 +236,6 @@ export default class Sketch {
         height: bounds.height,
       };
     });
-
-    console.log(this.imageStore);
   }
 
   setPosition() {
@@ -188,7 +246,7 @@ export default class Sketch {
   }
 
   addObjects() {
-    this.geometry = new THREE.PlaneBufferGeometry(200, 400, 10, 10);
+    this.geometry = new THREE.PlaneGeometry(200, 400, 10, 10);
     // this.geometry = new THREE.SphereBufferGeometry( 0.4, 40,40 );
     this.material = new THREE.MeshNormalMaterial();
 
@@ -220,7 +278,8 @@ export default class Sketch {
       m.uniforms.time.value = this.time;
     });
 
-    this.renderer.render(this.scene, this.camera);
+    //this.renderer.render(this.scene, this.camera);
+    this.composer.render();
     window.requestAnimationFrame(this.render.bind(this));
   }
 }
